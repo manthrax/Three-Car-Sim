11257585153029bfb4b67249c1b32fdaecc3fed5
Michael Schlachter <manthrax@gmail.com>
2026-04-22 04:17:46 -0700

    Move CSS to link tag to avoid MIME type errors

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 // ----------- MODULES ------------------------------------------------------------
-import './style.css';
 import * as THREE from 'three';
 import * as CANNON from 'cannon-es';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
